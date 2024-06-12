8e91385742f600d758f745c9ad40ccb37000f5f5
Devin Bost <devin.bost@gmail.com>
2024-06-12 08:51:58 -0500

    Added debug info to init

--- dual_encoder/__init__.py
+++ dual_encoder/__init__.py
@@ -1,7 +1,8 @@
-from .t5_contrastive import DualEncoderT5Contrastive, ContrastiveSentencePairDataset, ContrastiveDataModule, InfoNCELoss
-
-from .indexing_strategy import IndexingStrategy
-from .astradb_indexing import AstraDBIndexing
-from .faiss_indexing import FaissIndexing
-
-
+print("Initializing dual_encoder package")
+try:
+    from .t5_contrastive import DualEncoderT5Contrastive, ContrastiveSentencePairDataset, ContrastiveDataModule, InfoNCELoss
+    from .indexing_strategy import IndexingStrategy
+    from .astradb_indexing import AstraDBIndexing
+    from .faiss_indexing import FaissIndexing
+except Exception as e:
+    print("Error importing modules:", e)
